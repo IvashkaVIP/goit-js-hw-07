585cd0229d8ac378575514ed37ae9509c5a431db
IvashkaVIP <ivashka.co@gmail.com>
2023-04-27 15:10:08 +0300

    добавление слушателя кликов

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -4,8 +4,15 @@ import { galleryItems } from './gallery-items.js';
 const createImagesMarkup = (image) => {
     return image.map(({preview,original,description}) => {
      return `
-        <li>
-            <img src="${preview}" alt="${description}"/>
+        <li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                />
+            </a>
         </li>
     `;
     }).join('');
@@ -16,11 +23,26 @@ const galleryContainer = document.querySelector(".gallery");
 const imagesMarkup = createImagesMarkup(galleryItems);
 const galleryMarkup = galleryContainer.insertAdjacentHTML('beforeend', imagesMarkup);
 
-console.log(createImagesMarkup(galleryItems));
+galleryContainer.addEventListener('click', onGalleryContainerClick)
+
+function onGalleryContainerClick(evt) {
+    evt.preventDefault();
+     if (!evt.target.classList.contains('gallery__image')) {
+         return;
+     }
+    //const urlMainImage = evt.target.contains()
+    
+    console.log(evt.target.dataset.source);
+}
+
+
+//console.log(createImagesMarkup(galleryItems));
 
 //console.log(galleryItems);
 // preview:
 //       'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg',
 //     original:
 //       'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg',
-//     description: 'Alpine
+//     description: 'Alpin
+
+//insertAdjacentHTML

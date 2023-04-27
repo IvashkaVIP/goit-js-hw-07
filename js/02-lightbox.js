17914d38b69631d04420405cd4cee5aafb7623df
IvashkaVIP <ivashka.co@gmail.com>
2023-04-27 18:40:16 +0300

    Задание 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,35 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+
+const createImagesMarkup = (image) => {
+  return image
+    .map(({ preview, original, description }) => {
+      return `
+        <li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a>
+        </li>
+    `;
+    })
+    .join("");
+};
+
+const galleryContainer = document.querySelector(".gallery");
+const imagesMarkup = createImagesMarkup(galleryItems);
+const galleryMarkup = galleryContainer.insertAdjacentHTML(
+  "beforeend",
+  imagesMarkup
+);
+
+ let lightbox = new SimpleLightbox(".gallery a", {
+ });
+const lightboxOptions = lightbox.options;
+
+ lightboxOptions.captions = true;
+ lightboxOptions.captionDelay = 250;
+ lightboxOptions.captionsData = 'alt';
+
+//console.log(lightbox);
+

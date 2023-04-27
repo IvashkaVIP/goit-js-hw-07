beb359e039a1908e316ec110f24a621f3c8e831d
IvashkaVIP <ivashka.co@gmail.com>
2023-04-27 13:36:31 +0300

    markup gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,26 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const createImagesMarkup = (image) => {
+    return image.map(({preview,original,description}) => {
+     return `
+        <li>
+            <img src="${preview}" alt="${description}"/>
+        </li>
+    `;
+    }).join('');
+    
+}
+
+const galleryContainer = document.querySelector(".gallery");
+const imagesMarkup = createImagesMarkup(galleryItems);
+const galleryMarkup = galleryContainer.insertAdjacentHTML('beforeend', imagesMarkup);
+
+console.log(createImagesMarkup(galleryItems));
+
+//console.log(galleryItems);
+// preview:
+//       'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg',
+//     original:
+//       'https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg',
+//     description: 'Alpine
